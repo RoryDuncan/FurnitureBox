3ec06e062c0beb586feef98cdd64e0054efab4a0
Rory Duncan <robust.rory@gmail.com>
2016-08-31 18:48:45 +0000

    Add shopify data retrieval script

--- scripts/retrieve-data.js
+++ scripts/retrieve-data.js
@@ -1,39 +1,53 @@
 var Sync = require('sync');
 var json = require('jsonfile');
-var firebase = require("firebase");
+var ShopifyBuy = require('shopify-buy');
 var path = require('path');
 
 // Initialize Firebase
-const config = {
-  apiKey: "AIzaSyDts4s_P7FFNejDRQkfTxh5GQTTdc4M6O4",
-  authDomain: "value-link.firebaseapp.com",
-  databaseURL: "https://value-link.firebaseio.com",
-  storageBucket: "value-link.appspot.com"
-};
+
+var config = {
+  apiKey: '394e2c0a1422b7f78bc5942ac33fd132',
+  myShopifyDomain: 'rory-value-link',
+  appId: '6'
+}
+
+var shopClient = ShopifyBuy.buildClient(config);
 
 var prepend = "Value-Link Data: "
-var fireApp = firebase.initializeApp(config);
-var db = fireApp.database().ref("/collections");
+
 Sync(function(){
 
     // Synchronous action:
-  console.log(prepend + "Updating '/client/data.json'")
-  console.log(prepend + "Retrieving firebase data..");
-  db.once("value")
+  console.log(prepend + "Updating '/client/collections.json'")
+  console.log(prepend + "Retrieving Shopify Collections from '" + config.myShopifyDomain + "'.");
+  
+  
+  shopClient.fetchAll('collections')
+  .then(function (collections) {
+    console.log(collections);
+    json.writeFileSync(path.join("client/collections.json"), collections, {spaces: 2});
+  })
+  .catch(function () {
+    console.log('Request failed');
+  });
+  
+  
+  
+  // db.once("value")
     
-    .then((snapshot) => {
-      console.log(prepend+"Retrieved.");
-      console.log(prepend+"Saving...")
-      var data = snapshot.val();
-      json.writeFileSync(path.join("client/collections.json"), data, {spaces: 2});
-      console.log(prepend+"Saved.");
-      process.exit(0);
-    })
+  //   .then((snapshot) => {
+  //     console.log(prepend+"Retrieved.");
+  //     console.log(prepend+"Saving...")
+  //     var data = snapshot.val();
+  //     json.writeFileSync(path.join("client/collections.json"), data, {spaces: 2});
+  //     console.log(prepend+"Saved.");
+  //     process.exit(0);
+  //   })
     
-    .catch((err) => {
-      console.error(prepend, "firebase error:", err);
-      process.exit(1);
-    });
+  //   .catch((err) => {
+  //     console.error(prepend, "firebase error:", err);
+  //     process.exit(1);
+  //   });
   
 
 });
