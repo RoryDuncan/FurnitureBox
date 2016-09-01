846a4843dea36ea50d339789b18645d9101fecc7
Rory Duncan <robust.rory@gmail.com>
2016-09-01 07:44:18 +0000

    Rebuild data update system to use shopify products / collections.

---
+++ config/shopify.js
@@ -0,0 +1,8 @@
+// shopify config
+// scrub the git history and switch to a ENV setup if the repository
+// becomes public.
+module.exports = {
+  apiKey: '394e2c0a1422b7f78bc5942ac33fd132',
+  domain: 'rory-value-link.myshopify.com',
+  appId: '6'
+}
--- scripts/retrieve-shopify-data.js
+++ scripts/retrieve-shopify-data.js
@@ -1,36 +1,93 @@
+/*
+
+    This script retrieves our collections & products from the shopify store.
+    It then merges the products into their respective collections, and makes a named hash of them:
+    
+    data.collections[<collection_name>]
+    
+    data.collections[<collection_name>].products
+
+*/
 var Sync = require('sync');
 var json = require('jsonfile');
 var ShopifyBuy = require('shopify-buy').default;
 var path = require('path');
+var config = require("../config/shopify.js");
+var client = ShopifyBuy.buildClient(config);
 
-// Initialize config
 
-var config = {
-  apiKey: '394e2c0a1422b7f78bc5942ac33fd132',
-  domain: 'rory-value-link.myshopify.com',
-  appId: '6'
+
+
+const log = (...args) => {
+  args.unshift("Update Script:\t")
+  console.log.apply(console, args);
 }
 
-var shopClient = ShopifyBuy.buildClient(config);
+// resulting container that is saved to file
+const data = {
+  collections: {},
+  keys: []
+};
 
-var prepend = "Value-Link Data: "
+// functions
 
-Sync(function(){
-
-    // Synchronous action:
-  console.log(prepend + "Updating '/client/collections.json'")
-  console.log(prepend + "Retrieving Shopify Collections from '" + config.domain + "'.");
+const getProducts = () => {
   
+  log("Retrieving Products...");
   
-  shopClient.fetchAll('collections')
-  .then(function (collections) {
-    console.log(prepend+"Retrieved.");
-    console.log(prepend+"Saving...")
-    json.writeFileSync(path.join("client/collections.json"), collections, {spaces: 2});
-    console.log(prepend+"Saved.");
+  return Promise.all(data.keys.map((handle, i) => {
+    let collection = data.collections[handle]
+    let collection_id = collection.attrs.collection_id;
+    let q = client.fetchQueryProducts({collection_id})
+    log("Retrieving products from collection", handle);
+    
+    // client.fetchQuery('products', { collection_id: 456 }).then(products => {
+    
+    // save our products
+    q.then((products) => {
+      log(`collection '${handle}' retrieved.`)
+      data.collections[handle].products = products;
+    }).catch(caughtError)
+
+    return q;
+  
+  })); // end of promise.all
+}
+
+
+const caughtError = (err) => {
+  console.error('Something went wrong!', err);
+}
+
+const saveCollections = (collections) => {
+
+  collections.forEach((col, i) => {
+    data.keys.push(col.attrs.handle);
+    data.collections[col.attrs.handle] = col;
+  });
+  log("Collections retrieved.");
+  return getProducts();
+
+}
+
+// init
+
+
+
+// Synchronous action:
+Sync(() => {
+
+  log("Updating '/client/collections.json'")
+  log("Retrieving Shopify Collections from '" + config.domain + "'.");
+  
+
+  log("Getting Collections...");
+  let p = client.fetchAll('collections')
+  .catch(caughtError)
+  .then(saveCollections)
+  .then(() => {
+    json.writeFileSync(path.join("client/collections.json"), data, {spaces: 2});
+    log("Done! Check collections.json for the new data.");
     process.exit(0);
   })
-  .catch(function (err) {
-    console.error('Something went wrong!', err);
-  });
 });
