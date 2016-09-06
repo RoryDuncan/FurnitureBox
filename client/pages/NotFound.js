bf1b210811d411569608a7d93dfcda94ba5c9dc0
Rory Duncan <robust.rory@gmail.com>
2016-09-06 06:46:15 +0000

    Fix missing closing parenthesis

--- client/pages/NotFound.js
+++ client/pages/NotFound.js
@@ -17,7 +17,7 @@ export class NotFound extends React.Component {
         <p>404 Errors happen when a URL is incorrect, or a page that once existed was moved.</p>
         <h4>How to Continue</h4>
         <ul>
-          <li>Double-check that the URL is correct (i.e., <code>/shop</code> and not <code>/shpo</code></li>
+          <li>Double-check that the URL is correct (i.e., <code>/shop</code> and not <code>/shpo</code>)</li>
           <li>If the page <em>should</em> be available, <Link to="/contact">let us know you ran into this problem</Link></li>
           <li>Forget this ever happened. <Link to="/">Return to the main site</Link></li>
         </ul>
