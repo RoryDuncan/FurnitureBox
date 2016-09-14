9d01318b96fed541a9dc9f414922740ad18ae84d
Rory Duncan <robust.rory@gmail.com>
2016-09-14 06:52:28 +0000

    i18n Shop.js

--- client/pages/Shop.js
+++ client/pages/Shop.js
@@ -4,14 +4,21 @@ import s from '../components/styles/App.styl';
 import Page from '../components/Page';
 const cx = classnames.bind(s);
 import {Catalog} from '../components/Shop.js';
+import formatMessage from 'format-message';
 
+// i18n
+let pageTitle = formatMessage({
+  id: "shop:page_title",
+  default: "Shop",
+  description: "The Shop Page's title"
+})
 
 export class Shop extends React.Component {
   render() {
     return (
       <Page>
         <div className={cx('site-title')}>
-          <h1>Shop</h1>
+          <h1>{pageTitle}</h1>
         </div>
         <Catalog />
       </Page>
