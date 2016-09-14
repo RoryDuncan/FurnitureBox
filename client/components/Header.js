6b25025be76b19ce0592e2d60b7a4662307d3c36
Rory Duncan <robust.rory@gmail.com>
2016-09-14 16:33:01 +0000

    i18n Header.js

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,19 +1,66 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import { Link, IndexLink } from 'react-router';
+import formatMessage from 'format-message';
 
 // styling
 import classnames from 'classnames/bind';
 import s from './styles/Header.styl';
 const cx = classnames.bind(s);
 
+// i18n
+
+let brand = formatMessage({
+  id: "header:brand_name",
+  default: "Value-Link",
+  description: "The Brand Name"
+})
+
+let navLinks = {
+  
+  home: formatMessage({
+    id: "navigation:home_link_text",
+    default: "Home",
+    description: "The link text for navigating to the 'home' page."
+  }),
+  
+  shop: formatMessage({
+    id: "navigation:shop_link_text",
+    default: "Shop",
+    description: "The link text for navigating to the 'shop' page."
+  }),
+  
+  about: formatMessage({
+    id: "navigation:about_link_text",
+    default: "About",
+    description: "The link text for navigating to the 'about' page."
+  }),
+  
+  collections: formatMessage({
+    id: "navigation:collections_link_text",
+    default: "Collections",
+    description: "The link text for navigating to the 'collections' page."
+  }),
+  
+  faq: formatMessage({
+    id: "navigation:faq_link_text",
+    default: "FAQ",
+    description: "The link text for navigating to the 'faq' page."
+  }),
+  
+  contact: formatMessage({
+    id: "navigation:contact_link_text",
+    default: "Contact",
+    description: "The link text for navigating to the 'contact' page."
+  })
+}
 
 export class SiteHeader extends React.Component {
   render(){
     return (
       <div className={cx("site-header")}>
         <div className={cx("brand")}>
-          <i className="material-icons">widgets</i> Value-Link
+          <i className="material-icons">widgets</i> {brand}
         </div>
         <Navigation />
       </div>
@@ -27,22 +74,22 @@ export class Navigation extends React.Component {
       <nav className={cx('nav')}>
         <ol>
           <li>
-            <IndexLink to='/' activeClassName={cx('active')}>Home</IndexLink>
+            <IndexLink to='/' activeClassName={cx('active')}>{navLinks.home}</IndexLink>
           </li>
           <li>
-            <Link to='/shop' activeClassName={cx('active')}>Shop</Link>
+            <Link to='/shop' activeClassName={cx('active')}>{navLinks.shop}</Link>
           </li>
           <li>
-            <Link to='/about' activeClassName={cx('active')}>About</Link>
+            <Link to='/about' activeClassName={cx('active')}>{navLinks.about}</Link>
           </li>
           <li>
-            <Link to='/collections' activeClassName={cx('active')}>Collections</Link>
+            <Link to='/collections' activeClassName={cx('active')}>{navLinks.collections}</Link>
           </li>
           <li>
-            <Link to='/faq' activeClassName={cx('active')}>FAQ</Link>
+            <Link to='/faq' activeClassName={cx('active')}>{navLinks.faq}</Link>
           </li>
           <li>
-            <Link to='/contact' activeClassName={cx('active')}>Contact</Link>
+            <Link to='/contact' activeClassName={cx('active')}>{navLinks.contact}</Link>
           </li>
         </ol>
       </nav>
