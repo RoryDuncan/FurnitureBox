ed9e4272024fcf1aeecf1ad127a336a02896a7fc
Rory Duncan <robust.rory@gmail.com>
2016-09-14 13:58:58 +0000

    i18n Collection.js component

--- client/components/Collection.js
+++ client/components/Collection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import { Link } from 'react-router';
+import formatMessage from 'format-message';
 
 // styling
 import classnames from 'classnames/bind';
@@ -48,12 +49,20 @@ export const Collection = (props) => {
 // The collections shown on /shop
 export const CatalogCollection = (props) => {
 
+  // i18n
+  let learnAboutCollectionLinkText = formatMessage({
+    id: "collection:learn_more_link",
+    default: "Learn about the {name} Collection",
+    description: "Link text, linking to a collection page where the user can learn more about the collection."
+  }, {name: props.name})
+  
+
   return (
     <div className={cx("collection-wrapper")}>
       
       <h3>{props.name}</h3>
       <p>
-        <Link to={`/collections/${props.name}`}>Learn about the {props.name} Collection</Link>
+        <Link to={`/collections/${props.name}`}>{learnAboutCollectionLinkText}</Link>
       </p>
       <Collection name={props.name} products={props.products} />
 
@@ -76,6 +85,19 @@ export const CollectionList = () => {
 // The name, image, and description of a collection. Does not include items
 export const CollectionDescription = (props) => {
 
+  // i18n
+  let shopThisCollectionBtnText = formatMessage({
+    id: "collection:shop_this_collection_btn",
+    default: "Shop this Collection",
+    description: "Button text, taking the user to the shop where they can buy products of the current collection."
+  })
+  
+  let viewThisCollectionLinkText = formatMessage({
+    id: "collection:view_collection_link",
+    default: "View the {name} Collection",
+    description: "Link text, link the user to the collection's page."
+  }, {name: props.attrs.title})
+
   let src = "//placehold.it/1040x420";
   if (props.attrs.image !== null) {
     src = props.attrs.image.src;
@@ -87,12 +109,12 @@ export const CollectionDescription = (props) => {
       <img className={cx("collection-image")} src={src} />
       <div className={cx("shop-collection")}>
         <Link to={`/shop#${props.attrs.title.toLowerCase()}`}>
-          <button>Shop this Collection</button>
+          <button>{shopThisCollectionBtnText}</button>
         </Link>
       </div>
       <div className={cx("description")} dangerouslySetInnerHTML={props.description} />
       {props.linked === true ? 
-        <Link to={`/collections/${props.attrs.title.toLowerCase()}`} >View the {props.attrs.title} Collection</Link> 
+        <Link to={`/collections/${props.attrs.title.toLowerCase()}`} >{viewThisCollectionLinkText}</Link> 
         : false
       }
 
