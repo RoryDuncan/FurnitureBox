371722058a7a2d4f4c5f9ae8d50a612b9787cc0a
Rory Duncan <robust.rory@gmail.com>
2016-09-14 06:26:33 +0000

    i18n FAQ.js

--- client/pages/FAQ.js
+++ client/pages/FAQ.js
@@ -2,7 +2,28 @@ import React from 'react';
 import classnames from 'classnames/bind';
 import s from '../components/styles/App.styl';
 import Page from '../components/Page';
+import formatMessage from 'format-message';
 const cx = classnames.bind(s);
+
+
+// i18n
+let pageTitle = formatMessage({
+  id: "faq:page_title",
+  default: "Frequently Asked Questions",
+  description: "The Frequently Asked Questions Page's title"
+})
+
+let pageDescription = formatMessage({
+  id: "faq:description",
+  default: "Refer to the Questions and Answers below before. If you don't find your question contact us and we'll answer it.",
+  description: "The Frequently Asked Questions description text, describing what the FAQ is used for."
+})
+
+let faqEmptyMessage = formatMessage({
+  id: "faq:no_questions_yet",
+  default: "We haven't been asked very many question yet.",
+  description: "The Frequently Asked Questions page text, before we add questions and answers to it."
+})
 
 
 export class FAQ extends React.Component {
@@ -10,21 +31,11 @@ export class FAQ extends React.Component {
     return (
       <Page>
         <div className={cx('site-title')}>
-          <h1>Frequently Asked Questions</h1>
+          <h1>{pageTitle}</h1>
         </div>
-        <p>Lorum Ipsum</p>
+        <p>{pageDescription}</p>
         
-        <ul>
-          <li>
-            <p>Question Answer</p>
-          </li>
-          <li>
-            <p>Question Answer</p>
-          </li>
-          <li>
-            <p>Question Answer</p>
-          </li>
-        </ul>
+        <p>{faqEmptyMessage}</p>
       </Page>
     );
   }
