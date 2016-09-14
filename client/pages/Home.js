e65af9f413c11afe138cea486daf3a269e84066c
Rory Duncan <robust.rory@gmail.com>
2016-09-14 06:30:19 +0000

    i18n Home.js

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -3,14 +3,28 @@ import classnames from 'classnames/bind';
 import s from '../components/styles/App.styl';
 const cx = classnames.bind(s);
 import Page from '../components/Page';
+import formatMessage from 'format-message';
+
+let pageTitle = formatMessage({
+  id: "home:page_title",
+  default: "Value Link",
+  description: "The Home Page's title"
+})
+
+let pageDescription = formatMessage({
+  id: "home:page_description",
+  default: "This is the homepage of Value Link",
+  description: "The Home Page's description or Value Link's business motto"
+})
+
 
 export class Home extends React.Component {
   render() {
     return (
       <Page>
         <div className={cx('site-title')}>
-          <h1>Value Link</h1>
-          <p>This is the homepage of Value Link</p>
+          <h1>{pageTitle}</h1>
+          <p>{pageDescription}</p>
           <h2>Roadmap</h2>
           <p>The features and bugs that need apprehending</p>
           <ul>
