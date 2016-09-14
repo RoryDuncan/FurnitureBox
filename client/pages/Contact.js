f5a1cfdca95e1d12620a57a05837ed30cb0c1106
Rory Duncan <robust.rory@gmail.com>
2016-09-14 06:19:24 +0000

    i18n Contact.js

--- client/pages/Contact.js
+++ client/pages/Contact.js
@@ -3,8 +3,16 @@ import classnames from 'classnames/bind';
 import s from '../components/styles/App.styl';
 import Page from '../components/Page';
 const cx = classnames.bind(s);
+import formatMessage from 'format-message';
 
 import {ContactForm, StamplaySDK} from '../components/ContactForm';
+
+let pageTitle = formatMessage({
+  id: "contact:page_title",
+  default: "Contact",
+  description: "The Contact Page's title"
+})
+
 
 
 export class Contact extends React.Component {
@@ -16,7 +24,7 @@ export class Contact extends React.Component {
     return (
       <Page>
         <div className={cx('site-title')}>
-          <h1>Contact</h1>
+          <h1>{pageTitle}</h1>
         </div>
         <ContactForm action={action} method={method} />
       </Page>
