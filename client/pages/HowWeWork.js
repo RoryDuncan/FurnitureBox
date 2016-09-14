a7294ad7f1c3d0c1936ff85d40fe2371629af56b
Rory Duncan <robust.rory@gmail.com>
2016-09-14 06:35:45 +0000

    i18n HowWeWork.js

--- client/pages/HowWeWork.js
+++ client/pages/HowWeWork.js
@@ -2,17 +2,38 @@ import React from 'react';
 import classnames from 'classnames/bind';
 import s from '../components/styles/App.styl';
 import Page from '../components/Page';
+import formatMessage from 'format-message';
 const cx = classnames.bind(s);
 
+
+let pageTitle = formatMessage({
+  id: "how_we_work:page_title",
+  default: "How We Work",
+  description: "The How We Work Page's title"
+})
+
+let pageDescription = formatMessage({
+  id: "how_we_work:page_description",
+  default: "Learn the intricate processes of Value Link",
+  description: "The How We Work Page's description"
+})
+
+let contentParagraph = formatMessage({
+  id: "how_we_work:how_we_work_paragraph1",
+  default: "How We Work",
+  description: "The How We Work Page's title"
+})
 
 export class HowWeWork extends React.Component {
   render() {
     return (
       <Page>
         <div className={cx('site-title')}>
-          <h1>How We Work</h1>
+          <h1>{pageTitle}</h1>
         </div>
-        <p>Welcome to the how we work page...</p>
+        <p>{pageDescription}</p>
+        
+        <p>{contentParagraph}</p>
       </Page>
     );
   }
